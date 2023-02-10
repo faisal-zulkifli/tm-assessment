3d0bccf8c59c55741d46b39f14b3d62f2f60b9d9
faisal-zulkifli <faisal.zulkifli@accenture.com>
2023-02-10 16:09:06 +0800

    progress

---
+++ pages/api/userInput.js
@@ -0,0 +1,13 @@
+export default function userInput(req, res) {
+  if (req.method === "POST") {
+    const reqPayload = req?.body;
+
+    log("Req Payload", reqPayload);
+
+    res.json({ msg: "Test" });
+  }
+
+  return res.status(500).json({
+    msg: "Error, This need to be a respond",
+  });
+}
---
+++ pages/formInput.js
@@ -0,0 +1,128 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+
+export default function FormInput() {
+  
+  const [name, setname] = useState(""); 
+  const [email, setemail] = useState("");
+  const [mobile, setmobile] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userPayload = {
+      name,
+      email,
+      mobile,
+    };
+    
+    console.log("Payload: ", userPayload);
+
+    try {
+      const responseData = axios ({
+        url: "",
+        method:"POST",
+        data: userPayload
+
+      });
+
+      console.log("Response Back:", data);
+    } catch (error) {
+
+      console.log("Error:", data);
+
+    }
+  };
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Form
+          </Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            TM Frontend Junior Assessment
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel> Name</FormLabel>                  
+                  <Input
+                    type="text"
+                    value={name}
+                    required
+                    onChange={(e) => setname(e.target.value)}
+                  />
+                </FormControl>
+              </Box>              
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                placeholder="example@mail.com"
+                value={email}
+                required
+                onChange={(e) => setemail(e.target.value)}
+              />
+            </FormControl>
+            <FormControl id="mobile" isRequired>
+              <FormLabel>Mobile Number</FormLabel>
+              <Input
+                type="number"
+                placeholder="(+60) __-______"
+                value={mobile}
+                required
+                onChange={(e) => setmobile(e.target.value)}
+              />
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                onClick={handleSubmit}
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
